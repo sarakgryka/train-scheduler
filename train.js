d11801c7740c8584f742bf25de554282e49567ec
Sara Gryka <sarakgryka@hotmail.com>
2019-10-02 13:38:21 -0700

    bootstrap update

--- train.js
+++ train.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
 
 
+//Variables
 
+//initiate firebase
+
+
+
+// Assign attr to the lists
+
+// create function to calcute minutes away 
+
+
+
+//create set function
+
+//create get function
+
+
+
+//create function for val of form
+
+//onclick function for submit button 
 
 
 
